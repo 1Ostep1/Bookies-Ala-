102bd00d6d6c05d54594df4983090685cc6254eb
Ostep <ostep1@icloud.com>
2021-03-03 19:01:00 +0600

    updates

--- src/main/java/com/ostep/bookies/Controllers/LoginController.java
+++ src/main/java/com/ostep/bookies/Controllers/LoginController.java
@@ -1,15 +1,15 @@
-package com.ostep.bookies.Controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class LoginController {
-
-    @GetMapping("/login")
-    public String LogIn(Model model) {
-        model.addAttribute("title", "Bookies");
-        return "/login";
-    }
-}
+//package com.ostep.bookies.Controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class LoginController {
+//
+//    @GetMapping("/login")
+//    public String LogIn(Model model) {
+//        model.addAttribute("title", "Bookies");
+//        return "/login";
+//    }
+//}
